500cdd36e95959772a70d32ba2ce96a62677a358
calebjo <caleb.ri.jones@gmail.com>
2021-11-22 23:16:42 -0500

    Add methods in phase_1_2.js

--- Week 8/W8D2/phase_1.js
+++ Week 8/W8D2/phase_1.js
@@ -3,3 +3,39 @@ function madLib(verb, adjective, noun) {
 }
 
 // madLib('kill','bad', 'Batman');
+
+function isSubstring(searchString, subString){
+    return searchString.includes(subString);
+}
+
+// console.log(isSubstring('Cabitathabitat', 'cat'));
+// console.log(isSubstring('Cabitathabitat', 'abitat'));
+
+function fizzBuzz(array) {
+    var new_array = [];
+    for (i = 0; i < array.length; i++){
+        if (!(array[i] % 3 == 0 || array[i] % 5 == 0)){
+            new_array.push(array[i]);
+        }
+    }
+    // console.log(new_array);
+}
+
+// fizzBuzz([1,2,3,41,4,55,15]);
+
+function isPrime(number) {
+    if (number < 2) { return false }
+    for (i = 2; i < number; i++) {
+        if (number % i === 0){ return false }
+    }
+    return true
+}
+
+// console.log(isPrime(2));
+// console.log(isPrime(10));
+// console.log(isPrime(15485863));
+// console.log(isPrime(3548563));
+
+function sumOfNPrimes(n) {
+
+}
