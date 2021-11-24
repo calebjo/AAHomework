333479aad3c04282b6ccc51c1bebc50304bd653b
calebjo <caleb.ri.jones@gmail.com>
2021-11-23 19:14:02 -0500

    Add test methods for callbacks.js in W8D4

---
+++ Week 8/W8D4/callbacks.js
@@ -0,0 +1,21 @@
+// let callback = function() {
+//     console.log('HAMMERTIME!');
+// }
+// let timeToWait = 5000;
+
+// setTimeout(callback, timeToWait);
+
+// Because we can use the website as the setTimeout occurs, this
+// means that setTimeout is asynchronous.
+
+// function hammerTime(time){
+//     let callback = function() {
+//         console.log(`${time} is hammertime!`);
+//     }
+//     let timeToWait = time;
+    
+//     setTimeout(callback, timeToWait);
+// }
+
+// hammerTime(3000);
+
