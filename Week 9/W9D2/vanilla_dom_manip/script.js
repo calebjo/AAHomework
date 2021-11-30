a1ee0dca6b83082c0f04ce738177a03a92e7d586
calebjo <caleb.ri.jones@gmail.com>
2021-11-29 23:47:31 -0500

    Add event listener in script.js

--- Week 9/W9D2/vanilla_dom_manip/script.js
+++ Week 9/W9D2/vanilla_dom_manip/script.js
@@ -14,11 +14,27 @@ document.addEventListener("DOMContentLoaded", () => {
     li.addEventListener("click", toggleLi);
   });
 
-
-
   // adding SF places as list items
 
-  // --- your code here!
+  const placeForm = document.getElementById('place-form');
+
+  placeForm.addEventListener('submit', event => {
+    // *cancel default behavior for this event*
+    // if we don't include this line, the browser will do what it normally does
+    // when you submit a form: make an HTTP request, causing a full page refresh
+    event.preventDefault();
+
+    // Get the name input element, get the value, and clear it.
+    const placeNameInput = document.getElementById('favorite-input');
+    const placeName = placeNameInput.value;
+    placeNameInput.value = '';
+
+    // Add a new li with the sheep's name to the sheep ul.
+    const ul = document.getElementById('sf-places');
+    const li = document.createElement('li');
+    li.textContent = placeName;
+    ul.appendChild(li);
+  });
 
 
 
